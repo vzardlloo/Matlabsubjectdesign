09258ee2a97a88f5aa542f44a3186be9bc892254
Cailei Lu <vzardlloo@gmail.com>
2018-07-11 10:35:01 +0800

    Update Num01(PE12).m

--- Num01(PE12).m
+++ Num01(PE12).m
@@ -1,8 +1,8 @@
-triNum = 0; k = 0;count = 0;
-while count < 500
+triNum = 0; k = 0;count = 0;        //初始化数据
+while count < 500                   //判断约数数量
     k = k + 1;
-    triNum = triNum + k;
-    up = sqrt(triNum);   
-    count = sum(~mod(uint32(triNum), 1:uint32(up)))*2 - (up == floor(up));
+    triNum = triNum + k;            //根据上一个三角形数来构造当前三角形数
+    up = sqrt(triNum);          
+    count = sum(~mod(uint32(triNum), 1:uint32(up)))*2 - (up == floor(up));  //求出当前三角形数的约数数量
 end
 triNum
